75cb60acc6e19ea5618cc07e7a73010d5cc2c261
James Forino <57734046+jforino@users.noreply.github.com>
2025-05-03 19:23:41 -0700

    add required tag to task title

--- src/dom.js
+++ src/dom.js
@@ -4,11 +4,15 @@ import Project from './project.js'
 // Global var declares 
 const toDoListUL = document.getElementById('to-do-list-ul'); 
 const projectNameHeading = document.getElementById('project-name-div');
+const form = document.getElementById('user-task-input-form'); 
 
 function addItemToDOMList(element){
     const task = document.createElement('li'); 
     task.className = 'to-do-list-li'
     task.textContent = element.taskTitle; 
+    // task.addEventListener('mouseenter', () => {
+        // --- ADD IN CODE for HOVERING AND SHOWING TASK DETAILS ---
+    // })
     toDoListUL.append(task);
 
     const checkBox = document.createElement('input');
@@ -20,7 +24,6 @@ function addItemToDOMList(element){
         console.log('clicked'); 
         
         element.completed = checkBox.checked; 
-        console.log(element.completed); 
     })
 
     task.appendChild(checkBox); 
@@ -28,6 +31,18 @@ function addItemToDOMList(element){
 
 function setProjectName(currentProject){
     projectNameHeading.textContent = `Project Name: ${currentProject.projectName}`;    
+}
+
+function createToDoItemFromUserInput(){
+
+    const taskTitle = document.getElementById('input-field-taskTitle').value;
+    const dueDate = document.getElementById('input-due-date').value;
+    const dueTime = document.getElementById('input-due-time').value;
+    const description = document.getElementById('input-field-task-description').value;
+
+    const newTask = new toDoItem(taskTitle, description, dueDate);
+    return newTask; 
+
 }
 
 // main export function
@@ -38,5 +53,19 @@ export function renderDOM(currentProject){
     currentProject.projectToDoList.forEach(element => {
         addItemToDOMList(element);  
     });
+
+
+    // listen for user input and create task
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const userCreatedTask = createToDoItemFromUserInput();
+        
+        addItemToDOMList(userCreatedTask);         
+        currentProject.projectToDoList.push(userCreatedTask); 
+
+        console.log('form submitted');
+        
+        form.reset();
+    })
 
 }
